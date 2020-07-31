9927ec186a608c213295c43bf7f197910636b622
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-07-31 14:13:54 -0300

    feat(Web Page): add function to fetch fake board data

--- src/KanbanBoard.Frontend/src/services/kanbanService.ts
+++ src/KanbanBoard.Frontend/src/services/kanbanService.ts
@@ -3,6 +3,7 @@ import { Board } from '../models';
 export interface IKanbanService {
     getBoards(): Board[];
     postBoard(summary: string): void;
+    getBoard(id: number): Board | undefined;
 }
 
 const boardsData = {
@@ -11,6 +12,41 @@ const boardsData = {
         {
             id: 1,
             summary: 'Project Tasks',
+            lists: [
+                {
+                    id: 1,
+                    title: 'To Do',
+                    tasks: [
+                        {
+                            id: 1,
+                            summary: 'Task #1',
+                            tagColor: '9932CC',
+                        },
+                        {
+                            id: 2,
+                            summary: 'Task #2',
+                            tagColor: '9932CC',
+                        },
+                        {
+                            id: 3,
+                            summary: 'Task #3',
+                            tagColor: '9932CC',
+                        },
+                    ],
+                },
+                {
+                    id: 2,
+                    title: 'Doing',
+                    tasks: [
+                        {
+                            id: 4,
+                            summary:
+                                'Task #4 Progress: 90% Update docs: Add project description in README and links to definitions in docs directory',
+                            tagColor: 'FFEA31',
+                        },
+                    ],
+                },
+            ],
         },
         {
             id: 2,
@@ -29,5 +65,8 @@ export const kanbanServiceFactory = (): IKanbanService => ({
             ...boardsData.boards,
         ];
         boardsData.nextId += 1;
+    },
+    getBoard(id: number) {
+        return boardsData.boards.find((board) => board.id === id);
     },
 });
