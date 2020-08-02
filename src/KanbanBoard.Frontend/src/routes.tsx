dcb1f124afcbf399a22349db1a590defa75d9a38
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-02 19:05:06 -0300

    feat(Web Page): add route to login and signup

--- src/KanbanBoard.Frontend/src/routes.tsx
+++ src/KanbanBoard.Frontend/src/routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AppTemplate } from './pages';
+import { AppTemplate, AuthPage } from './pages';
 
 export const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact path="\/(login|signup)" component={AuthPage} />
                 <Route path="/" component={AppTemplate} />
             </Switch>
         </BrowserRouter>
