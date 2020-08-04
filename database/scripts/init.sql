aceda055c3bcba65d3481dd8e5fd0b674e941da7
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-04 17:24:30 -0300

    feat: add database initialization script
    add script to create all the required tables on the database

---
+++ database/scripts/init.sql
@@ -0,0 +1,70 @@
+create table if not exists Users (
+    id serial,
+    name varchar(100) not null,
+    email varchar(320) unique not null,
+    password varchar(75) not null,
+    createdOn timestamp(2) not null,
+    modifiedOn timestamp(2) not null,
+
+    constraint PK_Users_id primary key (id)
+);
+
+create table if not exists Boards (
+    id serial,
+    title varchar(150) not null,
+    createdOn timestamp(2) not null,
+    modifiedOn timestamp(2) not null,
+    createdBy serial not NULL,
+
+    constraint PK_Boards_id primary key (id),
+    constraint FK_Boards_createdBy foreign key (createdBy) references Users
+);
+
+create table if not exists BoardMembers (
+    userId serial not null,
+    boardId serial not null,
+    isAdmin boolean not null,
+    createdOn timestamp(2) not null,
+    modifiedOn timestamp(2) not null,
+
+    constraint PK_BoardMembers_userId_boardId primary key (userId, boardId),
+    constraint FK_BoardMembers_userId foreign key (userId) references Users,
+    constraint FK_BoardMembers_boardId foreign key (boardId) references Boards
+);
+
+create table if not exists Lists (
+    id serial,
+    boardId serial not null,
+    title varchar(150) not null,
+    createdOn timestamp(2) not null,
+    modifiedOn timestamp(2) not null,
+
+    constraint PK_Lists_id primary key (id),
+    constraint FK_Lists_boardId foreign key (boardId) references Boards
+);
+
+create table if not exists Tasks (
+    id serial,
+    summary text not null, 
+    description text not null,
+    tagColor varchar(6) not null,
+    createdOn timestamp(2) not null,
+    modifiedOn timestamp(2) not null,
+
+    constraint PK_Tasks_id primary key (id)
+);
+
+create table if not exists ListTasks (
+    listId serial not null,
+    taskId serial not null
+);
+
+create table if not exists MemberTasks (
+    userId serial not null,
+    boardId serial not null,
+    taskId serial not null,
+
+    constraint PK_MemberTasks_userId_boardId_taskId primary key (userId, boardId, taskId),
+    constraint FK_MemberTasks_userId_boardId foreign key (userId, boardId) references BoardMembers,
+    constraint FK_MemberTasks_taskId foreign key (taskId) references Tasks
+);
