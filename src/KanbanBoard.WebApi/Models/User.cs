6fca3ed1c85ba06a71792fa37f589f7939183832
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-04 19:52:08 -0300

    feat(Web API): create the User model

---
+++ src/KanbanBoard.WebApi/Models/User.cs
@@ -0,0 +1,10 @@
+namespace KanbanBoard.WebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
