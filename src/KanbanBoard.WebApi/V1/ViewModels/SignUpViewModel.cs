6732227f6acdacc997f85a165c5d59a9ae08c1b9
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-05 17:26:00 -0300

    feat(Web API): create the users endpoint
    add POST action to handle the user creation and a GET action to return a single user by id

---
+++ src/KanbanBoard.WebApi/V1/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+namespace KanbanBoard.WebApi.V1.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
