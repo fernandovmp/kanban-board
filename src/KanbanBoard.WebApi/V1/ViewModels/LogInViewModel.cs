982b246284bb1da897ad2097e105b73735529456
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-07 16:02:41 -0300

    feat(Web API): add login action

---
+++ src/KanbanBoard.WebApi/V1/ViewModels/LogInViewModel.cs
@@ -0,0 +1,8 @@
+namespace KanbanBoard.WebApi.V1.ViewModels
+{
+    public class LogInViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
