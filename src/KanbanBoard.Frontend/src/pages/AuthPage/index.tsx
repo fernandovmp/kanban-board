ada56bc6279b593c84fc6a66981a450b5facb9d9
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-08 17:50:15 -0300

    refactor(Web Page): move authpage validation schemas to another file

--- src/KanbanBoard.Frontend/src/pages/AuthPage/index.tsx
+++ src/KanbanBoard.Frontend/src/pages/AuthPage/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Route, useHistory } from 'react-router-dom';
-import * as yup from 'yup';
 import {
     fetchLogin,
     fetchSignUp,
@@ -16,16 +15,7 @@ import {
     SubmitButton,
     Title,
 } from './styles';
-
-type LoginFormData = {
-    email: string;
-    password: string;
-};
-
-type FormData = {
-    name?: string;
-    confirmPassword?: string;
-} & LoginFormData;
+import { FormData, loginSchema, signUpSchema } from './validations';
 
 const defaultFormData: FormData = {
     name: '',
@@ -33,33 +23,6 @@ const defaultFormData: FormData = {
     password: '',
     confirmPassword: '',
 };
-
-const loginSchema = yup.object().shape<LoginFormData>({
-    email: yup
-        .string()
-        .required('e-mail is required')
-        .email('insert a valid email'),
-    password: yup.string().trim().required('password is required').min(8),
-});
-
-const signUpSchema = yup.object().shape<FormData>({
-    name: yup
-        .string()
-        .trim()
-        .required('name is required')
-        .max(100, "name length should be at maximum 100 character's"),
-    email: yup
-        .string()
-        .required('e-mail is required')
-        .email('insert a valid email'),
-    password: yup.string().trim().required().min(8),
-    confirmPassword: yup
-        .string()
-        .trim()
-        .required()
-        .min(8)
-        .equals([yup.ref('password')], "passwords doesn't match"),
-});
 
 type ValidationError = {
     path: string;
