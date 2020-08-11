6d22c93f139af61aac3b53dadb309b2a604e0747
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-11 14:37:31 -0300

    refactor(Web Page): move the apiPost method logic to internal method

--- src/KanbanBoard.Frontend/src/services/kanbanApiService.ts
+++ src/KanbanBoard.Frontend/src/services/kanbanApiService.ts
@@ -47,10 +47,14 @@ function unauthorizedResponse() {
     };
 }
 
-export async function apiPost<TResponse = any>(
-    params: IApiActionParams
+export async function apiPost<TResponse = any>(params: IApiActionParams) {
+    return await _apiAction<TResponse>({ ...params, method: 'POST' });
+}
+
+async function _apiAction<TResponse = any>(
+    params: IApiFetchParams
 ): Promise<IApiResponse<TResponse>> {
-    const response = await apiFetch({ ...params, method: 'POST' });
+    const response = await apiFetch(params);
     if (response.status === 401) {
         return unauthorizedResponse();
     }
