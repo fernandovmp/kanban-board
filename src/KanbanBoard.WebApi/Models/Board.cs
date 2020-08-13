0d9e1706670d053579dcabe335918768b9d35721
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-13 17:01:23 -0300

    feat(Web API): add a method to get the board with it lists, members and tasks

--- src/KanbanBoard.WebApi/Models/Board.cs
+++ src/KanbanBoard.WebApi/Models/Board.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace KanbanBoard.WebApi.Models
 {
@@ -6,6 +7,8 @@ namespace KanbanBoard.WebApi.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public List<BoardMember> Members { get; set; } = new List<BoardMember>();
+        public List<KanbanList> Lists { get; set; } = new List<KanbanList>();
         public User CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime ModifiedOn { get; set; }
