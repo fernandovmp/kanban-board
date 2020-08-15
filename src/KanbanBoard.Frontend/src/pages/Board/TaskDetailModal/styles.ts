69ce84f6963cab920d1c88e0e3db49d1936b097f
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-15 15:49:31 -0300

    feat(Web Page): add modal to display the task details

---
+++ src/KanbanBoard.Frontend/src/pages/Board/TaskDetailModal/styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { Card } from '../../../components';
+
+export const ModalCard = styled(Card)`
+    display: flex;
+    background: #f0f0f0;
+    padding: 24px;
+    margin: 30vh;
+    height: 80%;
+    width: 100%;
+    position: relative;
+`;
+
+export const CloseButton = styled.img`
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export const TaskSummary = styled.h2`
+    margin: 0;
+`;
+
+interface ITaskTagProps {
+    color: string;
+}
+
+export const SummaryAndDescriptionSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    margin-right: 40px;
+    flex: 3;
+`;
+
+export const TaskTag = styled.div<ITaskTagProps>`
+    border-radius: 8px;
+    background-color: ${(props) => props.color};
+    height: 16px;
+    width: 50px;
+    margin: 4px 0;
+`;
+
+export const DescriptionWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+`;
+
+export const SectionTitle = styled.h3`
+    margin: 0;
+`;
+
+export const DescriptionCard = styled.div`
+    margin-top: 12px;
+    width: 100%;
+    padding: 12px;
+    background: white;
+    border-radius: 8px;
+    min-height: 100px;
+    font-size: 14px;
+`;
