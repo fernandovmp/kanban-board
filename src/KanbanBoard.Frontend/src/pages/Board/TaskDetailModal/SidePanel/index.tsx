3064bbcc3f0c82a27e0a4543db3919b8b239295c
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-08-24 16:11:31 -0300

    feat(Web Page): add assignment management menu

--- src/KanbanBoard.Frontend/src/pages/Board/TaskDetailModal/SidePanel/index.tsx
+++ src/KanbanBoard.Frontend/src/pages/Board/TaskDetailModal/SidePanel/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import assignmentIcon from '../../../../assets/assignment.svg';
 import deleteIcon from '../../../../assets/delete_outline.svg';
-import { Task } from '../../../../models';
+import { Task, User } from '../../../../models';
+import { EditAssignments } from '../EditAssignments';
 import {
     AssignedMemberName,
     AssignmentSectionTitle,
@@ -28,37 +29,51 @@ interface ISidePanelProps {
 
 export const SidePanel: React.FC<ISidePanelProps> = ({ task }) => {
     const [selectedTagColor, setSelectedTagColor] = useState('');
+    const [showAssignmentsModal, setShowAssignmentsModal] = useState(false);
+    const [taskAssignments, setTaskAssignments] = useState<User[]>([]);
 
     useEffect(() => setSelectedTagColor(`#${task?.tagColor}`), [task]);
+    useEffect(() => setTaskAssignments(task?.assignments ?? []), [task]);
 
     return (
-        <SidePanelWrapper>
-            <SectionTitle>Tag</SectionTitle>
-            <TagColorInput
-                value={selectedTagColor}
-                onChange={(e) => setSelectedTagColor(e.target.value)}
-            />
-            <TagColors>
-                {colors.map((color, index) => (
-                    <Color
-                        key={index}
-                        color={color}
-                        onClick={() => setSelectedTagColor(color)}
-                    />
+        <>
+            <SidePanelWrapper>
+                <SectionTitle>Tag</SectionTitle>
+                <TagColorInput
+                    value={selectedTagColor}
+                    onChange={(e) => setSelectedTagColor(e.target.value)}
+                />
+                <TagColors>
+                    {colors.map((color, index) => (
+                        <Color
+                            key={index}
+                            color={color}
+                            onClick={() => setSelectedTagColor(color)}
+                        />
+                    ))}
+                </TagColors>
+                <Button>
+                    <img src={deleteIcon} alt="Delete" /> DELETE TASK
+                </Button>
+                <AssignmentSectionTitle>
+                    <img src={assignmentIcon} alt="Assignments" /> Assignments
+                </AssignmentSectionTitle>
+                <Button onClick={() => setShowAssignmentsModal(true)}>
+                    EDIT ASSIGNMENTS
+                </Button>
+                {taskAssignments.map((assignedMember, index) => (
+                    <AssignedMemberName key={index}>
+                        {assignedMember.name}
+                    </AssignedMemberName>
                 ))}
-            </TagColors>
-            <Button>
-                <img src={deleteIcon} alt="Delete" /> DELETE TASK
-            </Button>
-            <AssignmentSectionTitle>
-                <img src={assignmentIcon} alt="Assignments" /> Assignments
-            </AssignmentSectionTitle>
-            <Button>EDIT ASSIGNMENTS</Button>
-            {task?.assignments.map((assignedMember, index) => (
-                <AssignedMemberName key={index}>
-                    {assignedMember.name}
-                </AssignedMemberName>
-            ))}
-        </SidePanelWrapper>
+            </SidePanelWrapper>
+            {showAssignmentsModal && task && (
+                <EditAssignments
+                    taskAssignments={taskAssignments}
+                    onAssignmentsChange={setTaskAssignments}
+                    onClose={() => setShowAssignmentsModal(false)}
+                />
+            )}
+        </>
     );
 };
